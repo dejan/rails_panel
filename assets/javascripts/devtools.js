d636612f7e811b0a83b5ff13de2ac0abc1783117
Dejan Simic <desimic@gmail.com>
2012-11-18 16:53:06 +0100

    fix image location

--- assets/javascripts/devtools.js
+++ assets/javascripts/devtools.js
@@ -1,5 +1,5 @@
 chrome.devtools.panels.create("Rails",
-                              "rails.png",
+                              "assets/images/rails.png",
                               "panel.html",
                               function(panel) { console.log("Panel created."); });
 
