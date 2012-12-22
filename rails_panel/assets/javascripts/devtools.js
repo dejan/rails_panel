698f7f3b8e002cb706520e6df6705ce0f868a133
Dejan Simic <desimic@gmail.com>
2012-12-22 17:59:08 +0100

    icons

--- rails_panel/assets/javascripts/devtools.js
+++ rails_panel/assets/javascripts/devtools.js
@@ -1,4 +1,4 @@
-chrome.devtools.panels.create("Rails",
+chrome.devtools.panels.create("RailsPanel",
                               "assets/images/rails.png",
                               "panel.html",
                               function(panel) { console.log("Panel created."); });
