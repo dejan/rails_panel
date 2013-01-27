a67e35a6995519565bb2cea6a28e9ae77e1e0230
Jorge Dias <jorge@mrdias.com>
2013-01-27 21:04:03 +0100

    Adds support for emacs

--- rails_panel/assets/javascripts/filters.js
+++ rails_panel/assets/javascripts/filters.js
@@ -2,18 +2,19 @@ angular.module('RailsPanel', []).
   filter('editorify', function() {
     return function(input) {
       var mapping = {
-        mvim: "mvim://open?url=file://%s&line=%d&column=%d", 
+        mvim: "mvim://open?url=file://%s&line=%d&column=%d",
         mate: "txmt://open?url=file://%s&line=%d&column=%d",
-        subl: "subl://open?url=file://%s&line=%d&column=%d"}
+        subl: "subl://open?url=file://%s&line=%d&column=%d",
+        emacs: "emacs://open?url=file://%s&line=%d&column=%d"
+      };
       var editor = localStorage.getItem("railspanel.editor");
-      var editorPrefix = mapping[editor]
+      var editorPrefix = mapping[editor];
       var out = sprintf(editorPrefix, input, 1, 1);
       return out;
-    }
+    };
   }).
   filter('normalizeViewPath', function() {
     return function(input) {
       return input.remove(/.*\/app\/views/);
-    }
+    };
   });
-
