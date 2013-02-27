fc074f634f1a07b3dc592eb39e57ac97aff1997b
Dejan Simic <desimic@gmail.com>
2013-02-27 22:28:28 +0100

    fix tests for Ruby 1.8

--- meta_request/test/meta_request/event_test.rb
+++ meta_request/test/meta_request/event_test.rb
@@ -6,8 +6,8 @@ describe MetaRequest::Event do
   describe 'process_action.action_controller' do
     describe 'status' do
       it 'sets it to 500 if payload has exception key present' do
-        event1 = MetaRequest::Event.new('process_action.action_controller', 10, 11, 1705, {status:'200'})
-        event2 = MetaRequest::Event.new('process_action.action_controller', 10, 11, 1705, {exception:'...'})
+        event1 = MetaRequest::Event.new('process_action.action_controller', 10, 11, 1705, {:status => '200'})
+        event2 = MetaRequest::Event.new('process_action.action_controller', 10, 11, 1705, {:exception => '...'})
         assert_equal '200', event1.payload[:status]
         assert_equal '500', event2.payload[:status]
       end
@@ -16,8 +16,8 @@ describe MetaRequest::Event do
     describe 'format' do
       # Rails 3.0.x support
       it 'sets it to first of formats if not set' do
-        event1 = MetaRequest::Event.new('process_action.action_controller', 10, 11, 1705, {format:'html'})
-        event2 = MetaRequest::Event.new('process_action.action_controller', 10, 11, 1705, {formats:['js']})
+        event1 = MetaRequest::Event.new('process_action.action_controller', 10, 11, 1705, {:format => 'html'})
+        event2 = MetaRequest::Event.new('process_action.action_controller', 10, 11, 1705, {:formats => ['js']})
         assert_equal 'html', event1.payload[:format]
         assert_equal 'js', event2.payload[:format]
       end
@@ -26,9 +26,10 @@ describe MetaRequest::Event do
 
   describe 'ignore_closed_tempfile_params' do
     it 'sets closed tempfiles in params to strings on create' do
-      payload = {params: {"user" => {"upload" => OpenStruct.new(tempfile: Tempfile.new('foo'))} } }.with_indifferent_access
+      tempfile = Tempfile.new('foo')
+      payload = {:params => {"user" => {"upload" => OpenStruct.new(:tempfile => tempfile)} } }.with_indifferent_access
       event_1 = MetaRequest::Event.new('process_action.action_controller', 10, 11, 1705, payload)
-      assert_equal payload[:params][:user][:upload].tempfile, event_1.payload[:params][:user][:upload].tempfile
+      assert_same tempfile, event_1.payload[:params][:user][:upload].tempfile
 
       payload[:params][:user][:upload].tempfile.close
       event_2 = MetaRequest::Event.new('process_action.action_controller', 10, 11, 1705, payload)
