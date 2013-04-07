f553842799e1b222b7f2d6214342549e2ed681d1
Eric Guo <eric.guo@sandisk.com>
2013-04-07 13:07:51 +0800

    Sublime in windows support

--- rails_panel/assets/javascripts/filters.js
+++ rails_panel/assets/javascripts/filters.js
@@ -5,11 +5,19 @@ angular.module('RailsPanel', []).
         mvim: "mvim://open?url=file://%s&line=%d&column=%d",
         mate: "txmt://open?url=file://%s&line=%d&column=%d",
         subl: "subl://open?url=file://%s&line=%d&column=%d",
+        sblm: "sblm:///%s",
         emacs: "emacs://open?url=file://%s&line=%d&column=%d",
         mine: "rubymine://open?url=file://%s&line=%d"}
       var editor = localStorage.getItem("railspanel.editor");
       var editorPrefix = mapping[editor]
-      var out = sprintf(editorPrefix, input, 1, 1);
+      if (editor === 'sblm') {
+        var out = sprintf(editorPrefix, input);
+        // remove sblm:///c:/git/Icome/app/views/homes/index.html.erb the second :
+        // so become sblm:///c/git/Icome/app/views/homes/index.html.erb
+        out = out.slice(0, 9) + out.slice(10, out.len);
+      } else {
+        var out = sprintf(editorPrefix, input, 1, 1);
+      }
       return out;
     }
   }).
