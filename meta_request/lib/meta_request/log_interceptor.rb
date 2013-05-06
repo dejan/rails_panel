55b2353b7fdb7030dcb1ad3f252418d7f845783a
Dejan Simic <desimic@gmail.com>
2013-05-06 05:06:41 +0200

    fix logging #33

--- meta_request/lib/meta_request/log_interceptor.rb
+++ meta_request/lib/meta_request/log_interceptor.rb
@@ -5,7 +5,7 @@ module MetaRequest
       define_method(level) do |message|
         dev_log = AppRequest.current && !AppRequest.current.events.empty? && caller.first =~ /#{Rails.root}/
         AppRequest.current.events << Event.new('meta_request.log', 0, 0, 0, {:line => message, :caller => caller.first, :level => level}) if dev_log
-        super(level)
+        super(message)
       end
     end
 
