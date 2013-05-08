104bce34fcc9ead78bc9ec3ed89e92003d16e7e0
Dejan Simic <desimic@gmail.com>
2013-05-08 08:44:42 +0200

    fix logger methods signatures #53

--- meta_request/lib/meta_request/log_interceptor.rb
+++ meta_request/lib/meta_request/log_interceptor.rb
@@ -3,23 +3,36 @@ require 'callsite'
 module MetaRequest
   module LogInterceptor
 
-    %w( debug info warn error fatal ).each do |level|
-      define_method(level) do |message|
-        begin
-          dev_log = AppRequest.current && !AppRequest.current.events.empty? && caller.first =~ /#{Rails.root}/
-          if dev_log
-            c = Callsite.parse(caller).first
-            payload = {:message => message, :level => level, :line => c.line, :filename => c.filename, :method => c.method}
-            AppRequest.current.events << Event.new('meta_request.log', 0, 0, 0, payload)
-          end
-        # MetaRequest bug should not break Rails
-        rescue Exception => e
-          MetaRequest.logger.fatal(e.message + "\n " + e.backtrace.join("\n "))
-        ensure
-          super(message)
-        end
-      end
+    def debug(message = nil, &block)
+      push_event(:debug, message)
+      super
     end
 
+    def info(message = nil, &block)
+      push_event(:info, message)
+      super
+    end
+
+    def error(message = nil, &block)
+      push_event(:error, message)
+      super
+    end
+
+    def fatal(message = nil, &block)
+      push_event(:fatal, message)
+      super
+    end
+    
+    private
+    def push_event(level, message)
+      dev_log = AppRequest.current && !AppRequest.current.events.empty? && caller[1] =~ /#{Rails.root}/
+      if dev_log
+        c = Callsite.parse(caller[1])
+        payload = {:message => message, :level => level, :line => c.line, :filename => c.filename, :method => c.method}
+        AppRequest.current.events << Event.new('meta_request.log', 0, 0, 0, payload)
+      end
+    rescue Exception => e
+      MetaRequest.logger.fatal(e.message + "\n " + e.backtrace.join("\n "))
+    end
   end
 end
