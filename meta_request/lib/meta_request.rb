b4d4de79d272d20a271cdaa332a125b63f1074cb
Dejan Simic <desimic@gmail.com>
2014-07-08 00:35:26 +0200

    oops forgot to autoload version file

--- meta_request/lib/meta_request.rb
+++ meta_request/lib/meta_request.rb
@@ -1,4 +1,5 @@
 module MetaRequest
+  autoload :VERSION,          "meta_request/version"
   autoload :Event,            "meta_request/event"
   autoload :AppRequest,       "meta_request/app_request"
   autoload :Storage,          "meta_request/storage"
