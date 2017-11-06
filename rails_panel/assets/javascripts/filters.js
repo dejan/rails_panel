e8d660a1060824b79c6422ef5350597542b13e0a
Brian Weaver <bdrums@gmail.com>
2017-11-06 10:07:29 -0500

    Add support for VSCode

--- rails_panel/assets/javascripts/filters.js
+++ rails_panel/assets/javascripts/filters.js
@@ -15,7 +15,9 @@ angular.module('RailsPanel', [])
         sblm: "sblm:///%s",
         emacs: "emacs://open?url=file://%s&line=%d&column=%d",
         atom: "atm://open?url=file://%s&line=%d&column=%d",
-        mine: "x-mine://open?file=%s&line=%d"}
+        mine: "x-mine://open?file=%s&line=%d",
+        vscode: "vscode://open?file=%s&line=%d"
+      }
       var editor = localStorage.getItem("railspanel.editor");
       var editorPrefix = mapping[editor]
       if (editor === 'sblm') {
