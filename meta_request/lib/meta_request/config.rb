6c4bffe0f769ffe1f92a866197b9fe972c2147ae
Jan Jedrychowski <jan.jedrychowski@globalapptesting.com>
2019-07-04 23:26:50 +0200

    Fix warnings - do not redefine methods

--- meta_request/lib/meta_request/config.rb
+++ meta_request/lib/meta_request/config.rb
@@ -1,7 +1,5 @@
 module MetaRequest
   class Config
-    attr_accessor :logger, :storage_pool_size
-
     # logger used for reporting gem's fatal errors
     def logger
       @logger ||= Logger.new(File.join(Rails.root, 'log', 'meta_request.log'))
